26ae4410e9ad027cc46b2b44d166e9f07425ddc3
acoutinhoa <acoutinhoa@gmail.com>
2017-05-01 18:32:25 -0300

    f7/img

--- i0/urls.py
+++ i0/urls.py
@@ -4,7 +4,9 @@ from i0.views import *
 urlpatterns = [
     url(r'^$', index0f, name='index0f'),
     # sites
-    url(r'^bg/a=(?P<a>[0-2])/b=(?P<b>bg)/c=(?P<c>([01r]{3}|r|pb|rgb|piet))/d=(?P<d>[0-1])/$', bg, name='bg'),
-    url(r'^f7/a=(?P<a>[0-2])/b=(?P<b>bg)/c=(?P<c>([01r]{3}|r|pb|rgb|piet))/d=(?P<d>[0-1])/$', f7, name='f7'),
-    url(r'^f7/$', f70, name='f70'),
+    url(r'^f7/$', f7, {'tp':'', 'a':'', 'b':'', 'c':'',}, name='f7'),
+    url(r'^f7/(?P<tp>rgb)/(?P<a>[0-2])/(?P<b>[0-1])/(?P<c>([01x]{3}|pb|rgb))/$', f7, name='f7'),
+    url(r'^bg/(?P<tp>rgb)/(?P<a>[0-2])/(?P<b>[0-1])/(?P<c>([01x]{3}|pb|rgb))/$', bg, name='bg'),
+    url(r'^f7/(?P<tp>img)/(?P<a>[0])/(?P<b>[0-1])/(?P<c>.+\.(gif|jpg|jpeg|bmp))/$', f7, name='f7'),
+    url(r'^bg/(?P<tp>img)/(?P<a>[0])/(?P<b>[0-1])/(?P<c>.+\.(gif|jpg|jpeg|bmp))/$', bg, name='bg'),
 ]
--- i0/views.py
+++ i0/views.py
@@ -4,98 +4,105 @@ from i0.models import *
 import string
 import random
 
-d7 = {
-	# atualiza
-	'a' : [
-		'0', # atualiza 0s
-		'1', # atualiza random()s
-		'2', # nao atualiza
-	],
-	# tipo
-	'b' : [
-		'bg', # bg
-		# 'img', # img
-	],
-	# cor / img 
-	'c' : {
-		'bg' : [
-			'rrr', # rgb random
-			'10r', # magenta/vermelho random
-			'00r', # azul/preto random
-			'r', # preto/branco random
-			'pb', # choice(p, b)
-			'rgb', # choice(r, g, b, k)
-			'piet', # choice(r, y, b, w)
-		],
-	},
-	# divisao
-	'd' : [
-		'0', # 50%
-		'1', # random()s
-		# '2', # phi
-	],
-}
-
 def index0f(request):
 	return HttpResponse('index0f')
 
-def bg(request, a, b, c, d):
-	tt = 'f7/bg'
-	t = 'f7/bg.html'
-	rl = def_a(a)
-	rgb = def_c(b, c)
-	url = reverse('f7', args=(a, b, c, d,))
-	return render(request, t, {'tt':tt, 'url':url, 'bg':rgb, 'rl':rl})
-
-def f7(request, a, b, c, d):
+def f7(request, tp, a, b, c):
+	# randomiza variaveis
+	if tp == '':
+		tp = random.choice(list(d7.keys()))
+		a = random.choice(d7[tp]['a'])
+		b = random.choice(d7[tp]['b'])
+		c = random.choice(d7[tp]['c'])
 	tt = 'f7'
 	t = 'f7/f7%s.html' % ( random.choice(['v', 'h']) )
-	d1, d2 = def_d(d)
-	url = reverse(b, args=(a, b, c, d,))
-	return render(request, t, {'tt':tt, 'url1':url, 'url2':url, 'v1':d1, 'v2':d2})
+	url = reverse('bg', args=(tp, a, b, c,))
+	v1, v2 = def_b(b)
+	return render(request, t, {'tt':tt, 'url1':url, 'url2':url, 'v1':v1, 'v2':v2})
 
-def f70(request):
-	# randomiza variaveis
-	a = random.choice(d7['a'])
-	b = random.choice(d7['b'])
-	c = random.choice(d7['c'][b])
-	d = random.choice(d7['d'])
-	return redirect(f7, a=a, b=b, c=c, d=d)
+def bg(request, tp, a, b, c):
+	tt = 'f7/bg'
+	t = 'f7/bg.html'
+	url = reverse('f7', args=(tp, a, b, c,))
+	rl = def_a(a)
+	bg, bgs = def_c(tp, c)
+	return render(request, t, {'tt':tt, 'url':url, 'bg':bg, 'bgs':bgs, 'rl':rl})
 
 def def_a(a):
-	if a == '1':
+	if a == '0':
+		a = ''
+	elif a == '1':
 		a = str(random.randint(5,20))
 	elif a == '2':
-		a = ''
+		a = '0'
 	return a
 
-def def_c(b, c):
-	if c == 'pb':
-		c = random.choice(['0', '1'])
-	elif c == 'rgb':
-		c = random.choice(['100', '010', '001', '0'])
-	elif c == 'piet':
-		c = random.choice(['1', random.choice(['100', '110', '001']) ])
-	cor = ''
-	for car in c:
-		if car == 'r':
-			car = str(random.randint(0, 255))
-		elif car == '1':
-			car = '255'
-		cor += '%s,' % (car)
-		if len(c) == 1:
-			cor = cor*3
-	cor = cor[:-1]
-	return cor
+def def_b(b):
+	if b == '0':
+		v1 = v2 = '50%'
+	elif b == '1':
+	    v1 = random.randint(25, 75)
+	    v2 = 100 - v1
+	    v1 = str(v1) + '%'
+	    v2 = str(v2) + '%'
+	return v1, v2
 
-def def_d(d):
-	if d == '0':
-		d1 = d2 = '50%'
-	elif d == '1':
-	    d1 = random.randint(25, 75)
-	    d2 = 100 - d1
-	    d1 = str(d1) + '%'
-	    d2 = str(d2) + '%'
-	return d1, d2
+def def_c(tp, c):
+	if tp == 'rgb':
+		if c == 'pb':
+			c = random.choice(['0', '1'])
+		elif c == 'rgb':
+			c = random.choice(['100', '010', '001', '0'])
+		elif c == 'piet':
+			c = random.choice(['1', random.choice(['100', '110', '001']) ])
+		bg = ''
+		for car in c:
+			if car == 'x':
+				car = str(random.randint(0, 255))
+			elif car == '1':
+				car = '255'
+			bg += '%s,' % (car)
+			if len(c) == 1:
+				bg = bg*3
+		bg = 'rgb(%s)' % (bg[:-1])
+		bgs = ''
+	elif tp == 'img':
+		bg = 'url(/static/i0/gif/%s) no-repeat' % (c)
+		bgs = '100vw 100vh'
+	return bg, bgs
 
 
+d7 = {
+	'rgb' : {
+		'a' : [
+			'0', # nao atualiza
+			'1', # atualiza random()s
+			'2', # atualiza 0s
+		],
+		'b' : [
+			'0', # 50%
+			'1', # random()s
+			# '2', # phi
+		],
+		'c' : [
+			'xxx', # rgb random
+			'10x', # magenta/vermelho random
+			'00x', # azul/preto random
+			'pb', # choice(p, b)
+			'rgb', # choice(r, g, b, k)
+		],
+	},
+	'img' : {
+		'a' : [
+			'0', # nao atualiza
+		],
+		'b' : [
+			'0', # 50%
+			'1', # random()s
+			# '2', # phi
+		],
+		'c' : [
+			'hasselhoffian-recursion.gif',
+		],
+	}
+}
