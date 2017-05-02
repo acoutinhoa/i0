7fcc66de43b1845cb0fd126255f0aa47bf30120d
acoutinhoa <acoutinhoa@gmail.com>
2017-05-02 02:33:40 -0300

    f7/piet

--- i0/urls.py
+++ i0/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     # sites
     url(r'^f7/$', f7, {'tp':'', 'a':'', 'b':'', 'c':'',}, name='f7'),
     url(r'^bg/$', bg, {'tp':'', 'a':'', 'b':'', 'c':'',}, name='bg'),
-    url(r'^f7/(?P<tp>rgb)/(?P<a>[0-2])/(?P<b>[0-1])/(?P<c>([01x]{3}|pb|rgb))/$', f7, name='f7'),
-    url(r'^bg/(?P<tp>rgb)/(?P<a>[0-2])/(?P<b>[0-1])/(?P<c>([01x]{3}|pb|rgb))/$', bg, name='bg'),
+    url(r'^f7/(?P<tp>rgb)/(?P<a>[0-2])/(?P<b>[0-1])/(?P<c>([01x]{3}|pb|rgb|piet))/$', f7, name='f7'),
+    url(r'^bg/(?P<tp>rgb)/(?P<a>[0-2])/(?P<b>[0-1])/(?P<c>([01x]{3}|pb|rgb|piet))/$', bg, name='bg'),
     url(r'^f7/(?P<tp>img)/(?P<a>[0])/(?P<b>[0-1])/(?P<c>.+\.(gif|jpg|jpeg|bmp))/$', f7, name='f7'),
     url(r'^bg/(?P<tp>img)/(?P<a>[0])/(?P<b>[0-1])/(?P<c>.+\.(gif|jpg|jpeg|bmp))/$', bg, name='bg'),
 ]
