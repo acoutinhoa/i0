a497eaf4c4896631acbc9c4331a25f855b9ae054
acoutinhoa <acoutinhoa@gmail.com>
2017-05-04 16:08:07 -0300

    f7/l8p

--- f7/urls.py
+++ f7/urls.py
@@ -2,14 +2,16 @@ from django.conf.urls import url
 from f7.views import *
 
 urlpatterns = [
-	url(r'^f7/$', f7, {'tp':'', 'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='f7'),
-    url(r'^bg/$', bg, {'tp':'', 'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='bg'),
-    url(r'^f7/(?P<tp>(rgb|img|piet|l8p))/$', f7, {'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='f7'),
-    url(r'^bg/(?P<tp>(rgb|img|piet|l8p))/$', bg, {'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='bg'),
-    url(r'^f7/(?P<tp>rgb)/(?P<a>[01x])/(?P<b>[0])/(?P<c>([01x]{3}|x|pb|rgb|piet|cmyx|def))/(?P<d>[vhx]\d*)/(?P<e>[01x])/$', f7, name='f7'),
-    url(r'^bg/(?P<tp>rgb)/(?P<a>[01x])/(?P<b>[0])/(?P<c>([01x]{3}|x|pb|rgb|piet|cmyx|def))/(?P<d>[vhx]\d*)/(?P<e>[01x])/$', bg, name='bg'),
-    url(r'^f7/(?P<tp>img)/(?P<a>[01x])/(?P<b>[0])/\[(?P<c>.+\.(gif|jpg|jpeg))\]/(?P<d>[vhx]\d*)/(?P<e>[01x])/$', f7, name='f7'),
-    url(r'^bg/(?P<tp>img)/(?P<a>[01x])/(?P<b>[0])/\[(?P<c>.+\.(gif|jpg|jpeg))\]/(?P<d>[vhx]\d*)/(?P<e>[01x])/$', bg, name='bg'),
-    url(r'^f7/(?P<tp>(piet|l8p))/(?P<a>[01x])/(?P<b>[01x]([01x]{0,3}|pb|def))/(?P<c>([01x]{3}|x|pb|rgb|piet|cmyx|def))/(?P<d>[vhx]\d*)/(?P<e>[01x])/$', f7, name='f7'),
-    url(r'^bg/(?P<tp>(piet|l8p))/(?P<a>[01x])/(?P<b>[01x]([01x]{0,3}|pb|def))/(?P<c>([01x]{3}|x|pb|rgb|piet|cmyx|def))/(?P<d>[vhx]\d*)/(?P<e>[01x])/$', bg, name='bg'),
+	url(r'^f7/$', f7, {'f7tp':'', 'l8p':'', 'tp':'', 'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='f7'),
+    url(r'^bg/$', bg, {'f7tp':'', 'l8p':'', 'tp':'', 'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='bg'),
+    url(r'^f7/(?P<f7tp>[012])(?P<l8p>[-01x]?)/$', f7, {'tp':'', 'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='f7'),
+    url(r'^bg/(?P<f7tp>[012])(?P<l8p>[-01x]?)/$', bg, {'tp':'', 'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='bg'),
+    url(r'^f7/(?P<f7tp>[01])(?P<l8p>[-01x]?)/(?P<tp>([01x]{3}|x|pb|rgb|piet|cmyx|def))/$', f7, {'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='f7'),
+    url(r'^bg/(?P<f7tp>[01])(?P<l8p>[-01x]?)/(?P<tp>([01x]{3}|x|pb|rgb|piet|cmyx|def))/$', bg, {'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='bg'),
+    url(r'^f7/(?P<f7tp>[01])(?P<l8p>[-01x]?)/(?P<tp>([01x]{3}|x|pb|rgb|piet|cmyx|def))/(?P<a>[01x])/(?P<b>[01x])/(?P<c>\d+)/(?P<d>[01x])/(?P<e>[01x])/$', f7, name='f7'),
+    url(r'^bg/(?P<f7tp>[01])(?P<l8p>[-01x]?)/(?P<tp>([01x]{3}|x|pb|rgb|piet|cmyx|def))/(?P<a>[01x])/(?P<b>[01x])/(?P<c>\d+)/(?P<d>[01x])/(?P<e>[01x])/$', bg, name='bg'),
+    url(r'^f7/(?P<f7tp>2)(?P<l8p>[-01x]?)/-(?P<tp>.+\.(gif|jpg|jpeg|png))-/$', f7, {'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='f7'),
+    url(r'^bg/(?P<f7tp>2)(?P<l8p>[-01x]?)/-(?P<tp>.+\.(gif|jpg|jpeg|png))-/$', bg, {'a':'', 'b':'', 'c':'', 'd':'', 'e':'', }, name='bg'),
+    url(r'^f7/(?P<f7tp>2)(?P<l8p>[-01x]?)/-(?P<tp>.+\.(gif|jpg|jpeg|png))-/(?P<a>[0])/(?P<b>[0])/(?P<c>\d+)/(?P<d>[01x])/(?P<e>[01x])/$', f7, name='f7'),
+    url(r'^bg/(?P<f7tp>2)(?P<l8p>[-01x]?)/-(?P<tp>.+\.(gif|jpg|jpeg|png))-/(?P<a>[0])/(?P<b>[0])/(?P<c>\d+)/(?P<d>[01x])/(?P<e>[01x])/$', bg, name='bg'),
 ]
--- f7/views.py
+++ f7/views.py
@@ -4,63 +4,85 @@ from django.urls import reverse
 import string
 import random
 
-def f7(request, tp, a, b, c, d, e):
-	# randomiza
-	tp, a, b, c, d, e = def_var(tp, a, b, c, d, e)
-	# define
-	tt = 'f7/%s' % (tp)
-	url = reverse('bg', args=(tp, a, b, c, d, e))
-	if tp == 'piet':
-		f = def_f(b, c, d[0], e)
-		return render(request, 'f7/f7.html', {'tt':tt, 'url1':url, 'url2':url, 'f7':f})
-	elif tp == 'l8p':
-		f = def_f(b, c, d[0], e)
-		url2 = reverse('f7', args=(tp, a, b, c, d, e))
-		return render(request, 'f7/f7.html', {'tt':tt, 'url1':url2, 'url2':url, 'f7':f})
+def f7(request, f7tp, l8p, tp, a, b, c, d, e):
+	f7tp, l8p, tp, a, b, c, d, e = def_var(f7tp, l8p, tp, a, b, c, d, e)
+	tt = 'f7/%s' % (f7tp)
+	url0 = url1 = reverse('bg', args=(f7tp, l8p, tp, a, b, c, d, e))
+	# l8p
+	if l8p == 'x':
+		l8p = random.choice(['0', '1'])
+	if l8p == '0':
+		url0 = reverse('f7', args=(f7tp, l8p, tp, a, b, c, d, e))
+	elif l8p == '1':
+		url1 = reverse('f7', args=(f7tp, l8p, tp, a, b, c, d, e))
+	# html framset
+	if f7tp == '1':
+		t = 'f7/f7.html'
+		d = def_d(f7tp, d)
+		b, bc = def_b(tp, b)	
+		v0, v1 = def_e(f7tp, e)
+		if b == '1':
+			f = '%s="%s,%s" frameborder="%s" border="10" bordercolor="%s"' % (d, v0, v1, b, def_hxc(bc))
+		else:
+			f = '%s="%s,%s" frameborder="%s"' % (d, v0, v1, b)
+		return render(request, t, {'tt':tt, 'url0':url0, 'url1':url1, 'f7':f})
+	# html iframe
 	else:
-		t = def_d(d[0])
-		v1, v2 = def_e(e)
-		return render(request, t, {'tt':tt, 'url1':url, 'url2':url, 'v1':v1, 'v2':v2})
+		t = 'f7/f7%s.html' % (def_d(f7tp, d))
+		b, bc = def_b(tp, b)
+		s0 = def_bs(b, bc)
+		s1 = def_bs(b, bc)
+		v0, v1 = def_e(f7tp, e)
+		return render(request, t, {'tt':tt, 'url0':url0, 'url1':url1, 'v0':v0, 'v1':v1, 's0':s0, 's1':s1})
 
-def bg(request, tp, a, b, c, d, e):
-	# randomiza
-	tp, a, b, c, d, e = def_var(tp, a, b, c, d, e)
-	# define
+def bg(request, f7tp, l8p, tp, a, b, c, d, e):
+	f7tp, l8p, tp, a, b, c, d, e = def_var(f7tp, l8p, tp, a, b, c, d, e)
 	t = 'f7/bg.html'
-	tt = 'f7/bg/%s' % (tp)
-	url = reverse('f7', args=(tp, a, b, c, d, e))
+	tt = 'f7/bg/%s' % (f7tp)
+	url = reverse('f7', args=(f7tp, l8p, tp, a, b, c, d, e))
 	rl = def_a(a)
-	bg, bgs = def_c(tp, c)
+	bg, bgs = def_tp(f7tp, tp)
 	return render(request, t, {'tt':tt, 'url':url, 'bg':bg, 'bgs':bgs, 'rl':rl})
 
 # defs
-def def_var(tp, a, b, c, d, e):
-	if tp == '':
-		tp = random.choice(list(d7.keys()))
-	if a == '':
-		a = random.choice(d7[tp]['a'])
-		b = random.choice(d7[tp]['b'])
-		c = random.choice(d7[tp]['c'])
-		d = random.choice(d7[tp]['d'])
-		e = random.choice(d7[tp]['e'])
-	if c == 'def':
-		c = def_xxx()
+def def_var(f7tp, l8p, tp, a, b, c, d, e):
+	if f7tp == '':
+		f7tp = random.choice(d7tp)
+
+	if l8p == '':
+		l8p = random.choice(['-', random.choice(['-', random.choice(d7x)])])
+
+	if f7tp == '2':
+		if tp == '':
+			tp = random.choice(d7img)
+		if a == '':
+			a = '0'
+			b = '0'
+	else:
+		if tp == '':
+			tp = random.choice(d7cor)
+		if a == '':
+			a = random.choice(['x', random.choice(d7x)])
+			b = random.choice(d7x)
+
+	if c == '': 
+		c = '0'
+		d = random.choice(['x', random.choice(d7x)])
+		e = random.choice(d7x)
+	else:
+		c = str(int(c)+1)
+
+	if tp == 'def':
+		tp = def_xxx()
 	if b[1:] == 'def':
 		b = b[0] + def_xxx()
-	if len(d) == 1:
-		dn = '0'
-		d += dn
-	else:
-		dn = int(d[1:])+1
-		d = d[0]+str(dn)
-	if tp == 'img' and c[-3:] == 'gif' and a != '0':
-		a == '0'
-	return tp, a, b, c, d, e
+
+	return f7tp, l8p, tp, a, b, c, d, e
 
 def def_xxx():
 	cor = ''
 	for i in range(3):
-		cor += random.choice(['0', '1', 'x',])
+		cor += random.choice(d7x)
 	if 'x' not in cor:
 		if cor == '000' or cor == '111':
 			cor = 'x'
@@ -70,29 +92,49 @@ def def_xxx():
 
 def def_rgb(c):
 	cor = ''
-	for k in c:
-		if k == 'x':
-			k = str(random.randint(0, 255))
-		elif k == '1':
-			k = '255'
-		cor += '%s,' % (k)
+	for i in c:
+		if i == 'x':
+			i = str(random.randint(0, 255))
+		elif i == '1':
+			i = '255'
+		cor += '%s,' % (i)
 	if len(c) == 1:
 		cor = cor*3
 	return 'rgb(%s)' % (cor[:-1])
 
 def def_hxc(c):
 	cor = ''
-	for k in c:
-		if k == 'x':
-			for i in range(2):
+	for i in c:
+		if i == 'x':
+			for j in range(2):
 				cor += random.choice(list(string.hexdigits))
-		elif k == '0':
+		elif i == '0':
 			cor += '00'
-		elif k == '1':
+		elif i == '1':
 			cor += 'ff'
 	if len(c) == 1:
 		cor = cor*3
 	return '#%s' % (cor)
+
+def def_tp(f7tp, tp):
+	if f7tp == '2':
+		if tp[-3:] == 'gif':
+			bg = 'url(%s) no-repeat' % (tp)
+			bgs = '100vw 100vh'
+		else:
+			bg = 'url(%s) no-repeat center fixed' % (tp)
+			bgs = 'cover'
+		return bg, bgs
+	else:
+		if tp == 'pb':
+			tp = random.choice(['0', '1'])
+		elif tp == 'rgb':
+			tp = random.choice(['100', '010', '001', '0'])
+		elif tp == 'piet':
+			tp = random.choice(['1', random.choice(['100', '110', '001']) ])
+		elif tp == 'cmyx':
+			tp = random.choice(['011', '101', '110', random.choice(['0', '1']) ])
+		return def_rgb(tp), ''
 
 def def_a(a):
 	if a == '0':
@@ -102,152 +144,122 @@ def def_a(a):
 	elif a == 'x':
 		return str(random.randint(5,20))
 
-def def_c(tp, c):
-	if tp == 'img':
-		if c[-3:] == 'gif':
-			bg = 'url(%s) no-repeat' % (c)
-			bgs = '100vw 100vh'
-		else:
-			bg = 'url(%s) no-repeat center fixed' % (c)
-			bgs = 'cover'
-		return bg, bgs
-	else:
-		if c == 'pb':
-			c = random.choice(['0', '1'])
-		elif c == 'rgb':
-			c = random.choice(['100', '010', '001', '0'])
-		elif c == 'piet':
-			c = random.choice(['1', random.choice(['100', '110', '001']) ])
-		elif c == 'cmyx':
-			c = random.choice(['011', '101', '110', random.choice(['0', '1']) ])
-		return def_rgb(c), ''
-
-def def_d(d):
-	if d == 'x':
-		d = random.choice(['v', 'h'])
-	return 'f7/f7%s.html' % (d)
-
-def def_e(e):
-	if e == '0':
-		v1 = v2 = 50
-	elif e == '1':
-		phi = [38.1966, 61.8034]
-		v1 = phi.pop(random.randint(0, 1))
-		v2 = phi[0]
-	elif e == 'x':
-	    v1 = random.randint(25, 75)
-	    v2 = 100 - v1
-	return str(v1) + '%', str(v2) + '%'
-
-def def_f(b, c, d, e):
-	# divisao
-	if d == 'x':
-		d = random.choice(['v', 'h'])
-	if d == 'v':
-		d = 'cols'
-	elif d == 'h':
-		d = 'rows'
-	# proporcao
-	if e == '0':
-		e = 50
-	elif e == '1':
-		e = random.choice([40, 60])
-	elif e == 'x':
-	    e = random.randint(25, 75)
-	# borda
+def def_b(tp, b):
 	bc = ''
 	if len(b) > 1:
-		bc = b[1:]
 		b = b[0]
+		bc = b[1:]
 	if b == 'x':
 		b = random.choice(['0', '1'])
 	if b == '1':
 		# cor borda
 		if bc == '':
-			if c == 'pb':
+			if tp == 'pb':
 				bc = 'x'
-			elif c == 'rgb':
+			elif tp == 'rgb':
 				bc = '1'
-			elif c == 'piet':
+			elif tp == 'piet':
 				bc = '0'
-			elif c == 'xxx':
-				bc = c
+			elif tp == 'xxx':
+				bc = tp
 			else:
 				bc = random.choice(['0', '1'])
 		elif bc == 'pb':
 			bc = random.choice(['0', '1'])
-		return '{0}="{1}%,*" frameborder="{2}" border="10" bordercolor="{3}"'.format(d, str(e), b, def_hxc(bc))
+	return b, bc
+
+def def_bs(b, bc):
+	if b == '1':
+		s = ''
+		b = ['border-top: %s;', 'border-bottom: %s;', 'border-left: %s;', 'border-right: %s;', ]
+		for i, bt in enumerate(b):
+			o = random.randint(0, 1)
+			if o:
+				t = '10px solid %s' % (def_hxc(bc))
+				s += b[i] % (t)
+			else:
+				s += b[i] % ('0')
+		s += 'box-sizing: border-box;'
 	else:
-		return '{0}="{1}%,*" frameborder="{2}"'.format(d, str(e), b)
+		s = ''
+	return s
+
+def def_d(f7tp, d):
+	if d == 'x':
+		d = random.choice(['0', '1'])
+	if f7tp == '1':
+		if d == '0':
+			return 'rows'
+		elif d == '1':
+			return 'cols'
+	else:
+		return d
+
+def def_e(f7tp, e):
+	if e == '0':
+		v0 = 50
+	elif e == '1':
+		v0 = random.choice([40, 60])
+		# v0 = random.choice([38.1966, 61.8034])
+	elif e == 'x':
+	    v0 = random.randint(25, 75)
+	if f7tp == '1':
+		return str(v0) + '%', '*'
+	else:
+		v1 = 100-v0
+		return str(v0) + '%', str(v1) + '%'
 
 # variaveis
-# atualiza
-d7a = [
-	'0', # nao atualiza
-	'1', # atualiza 0s
-	'x', # atualiza random()s
-]
-# borda
-d7b = [
-	'0', # sem borda
-	'1', # com borda
-	'x', # random
-]
-# cor / img
-d7c = [
-	'piet', # choice(r, y, b, w)
-	'pb', # choice(p, b)
-	'rgb', # choice(r, g, b, k)
-	'cmyx', # choice(c, m, y, x)
-	'xxx', # rgb random
-	'10x', # magenta/vermelho random
-	'00x', # azul/preto random
-	'def', # def_xxx
-]
-# divisao
-d7d = [
-	'x', # /choice(v, h)
-	'v', # /vertical
-	'h', # /horizontal
-]
-# proporcao
-d7e = [
-	'0', # 50%
-	'1', # phi
-	'x', # random
-]
 
-d7 = {
-	'rgb' : {
-		'a' : d7a,
-		'b' : d7b[0],
-		'c' : d7c,
-		'd' : d7d,
-		'e' : d7e,
-	},
-	'img' : {
-		'a' : d7a[0],
-		'b' : d7b[0],
-		'c' : [
-			'/static/f7/hasselhoffian-recursion.gif', 
-			'/static/f7/guido-van-rossum_python.jpg',
-		],
-		'd' : d7d[0],
-		'e' : d7e,
-	},
-	'piet' : {
-		'a' : d7a,
-		'b' : d7b,
-		'c' : d7c,
-		'd' : d7d,
-		'e' : d7e,
-	},
-	'l8p' : {
-		'a' : d7a,
-		'b' : d7b,
-		'c' : d7c,
-		'd' : d7d,
-		'e' : d7e,
-	},
-}
+# f7tp = tipo
+# l8p = loop
+	# - = sem l8p
+	# 0 = l8p url0
+	# 1 = l8p url1
+	# x = l8p url0/url1
+# tp = cor/img
+# a = atualiza
+	# 0 = nao atualiza
+	# 1 = 0s
+	# x = 5-20s
+# b = borda
+	# 0 = sem borda
+	# 1 = com borda
+	# x = 0/1
+# c = contador
+# d = divisao
+	# 0 = horizontal
+	# 1 = vertical
+	# x = 0/1
+# e = proporcao
+	# 0 = 50%
+	# 1 = phi
+	# x = 25-75%
 
+# f7tp
+d7tp = [
+	'0', # iframe
+	'1', # frameset
+	'2', # iframe img
+]
+# tp
+d7img = [
+		'/static/f7/hasselhoffian-recursion.gif', 
+		'/static/f7/guido-van-rossum_python.jpg',
+]
+d7cor = [
+		'piet', # choice(r, y, b, w)
+		'pb', # choice(p, b)
+		'rgb', # choice(r, g, b, k)
+		'cmyx', # choice(c, m, y, choice(p, b))
+		'xxx', # rgb random
+		'10x', # magenta/vermelho random
+		'00x', # azul/preto random
+		'def', # def_xxx
+]
+# 01x
+d7x = [
+	'0',
+	'1',
+	'x',
+]
